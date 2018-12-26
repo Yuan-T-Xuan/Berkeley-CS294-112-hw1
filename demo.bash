66a0bb844c718f86c5697d1c7964e5774f4c5206
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2018-12-26 18:15:01 -0500

    reduce num_demonstrations to see how perf drops

--- demo.bash
+++ demo.bash
@@ -2,5 +2,5 @@
 set -eux
 for e in Hopper-v2 Ant-v2 HalfCheetah-v2 Humanoid-v2 Reacher-v2 Walker2d-v2
 do
-    python3 run_expert.py experts/$e.pkl $e --render --num_rollouts=100
+    python3 run_expert.py experts/$e.pkl $e --render --num_rollouts=5
 done
