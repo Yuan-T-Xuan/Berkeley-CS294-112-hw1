3952377111818579dea41305dbecc7c80a18ac37
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2018-12-27 22:41:36 -0500

    accelerate sampling & test on Humanoid-v2

--- dagger.py
+++ dagger.py
@@ -1,5 +1,4 @@
 import pickle
-import random
 import tensorflow as tf
 import numpy as np
 import gym
@@ -7,14 +6,14 @@ import gym
 from my_util import *
 import load_policy
 
-init_model_path = "/Users/xuan/Documents/DeepRL/Berkeley-CS294-112-hw1/selected_models/Hopper-v2-100000.ckpt"
-envname = "Hopper-v2"
-init_expert_data_path = "expert_data/Hopper-v2.pkl"
-expert_policy_path = "experts/Hopper-v2.pkl"
-dagger_iter = 10
-train_iter = 20000
-input_dim = 11
-output_dim = 3
+init_model_path = "/Users/xuan/Documents/DeepRL/Berkeley-CS294-112-hw1/selected_models/Humanoid-v2-100000.ckpt"
+envname = "Humanoid-v2"
+init_expert_data_path = "expert_data/Humanoid-v2.pkl"
+expert_policy_path = "experts/Humanoid-v2.pkl"
+dagger_iter = 15
+train_iter = 40000
+input_dim = 376
+output_dim = 17
 
 # create model, define loss, etc.
 input_ph, output_ph, output_pred = create_model(input_dim, output_dim)
@@ -26,15 +25,15 @@ saver = tf.train.Saver()
 f = open(init_expert_data_path, 'rb')
 p = pickle.load(f)
 f.close()
-observations = list(p['observations'])
-actions = list(p['actions'].reshape((p['actions'].shape[0], -1)))
-expert_data = list(zip(observations, actions))
+observations = p['observations']
+actions = p['actions'].reshape((p['actions'].shape[0], -1))
 
 # load expert policy
 expert_policy_fn = load_policy.load_policy(expert_policy_path)
 
-curr_data = expert_data
 env = gym.make(envname)
+mean_reward = list()
+std_reward = list()
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     saver.restore(sess, init_model_path)
@@ -44,9 +43,9 @@ with tf.Session() as sess:
         # train/fine-tune current model based on the dataset
         # for simplicity, fix batch_size = 100
         for train_step in range(train_iter+1):
-            random.shuffle(curr_data)
-            input_batch = np.array([s[0] for s in curr_data[:100]])
-            output_batch = np.array([s[1] for s in curr_data[:100]])
+            indices = np.random.randint(low=0, high=len(observations), size=100)
+            input_batch = observations[indices]
+            output_batch = actions[indices]
             _, currloss = sess.run(
                 [opt, loss],
                 feed_dict={
@@ -79,11 +78,16 @@ with tf.Session() as sess:
         print('rewards', rewards)
         print('mean reward', np.mean(rewards))
         print('std of reward', np.std(rewards))
+        mean_reward.append(float(np.mean(rewards)))
+        std_reward.append(float(np.std(rewards)))
         # query expert policy to get correct actions for collected observations
         aggr_actions = list()
         for obs in aggr_observations:
             action = expert_policy_fn(obs[None,:])[0]
             aggr_actions.append(action)
         # merge aggr data with current data
-        curr_data = curr_data + list(zip(aggr_observations, aggr_actions))
+        observations = np.append(observations, np.array(aggr_observations), axis=0)
+        actions = np.append(actions, np.array(aggr_actions), axis=0)
 
+print(mean_reward)
+print(std_reward)
